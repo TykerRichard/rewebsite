1fb3804af26c55cf3619f962caadb17f20a32f48
Tyler R <101682347+TykerRichard@users.noreply.github.com>
2023-11-03 12:58:08 -0500

    first push
    landing page

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+
+export PATH="/usr/local/lib/node_modules:$PATH"
---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import rebanner from './rebanner.jpeg'
+
+function NavBar(){
+    
+
+return(
+    <><div className='Navbar'>
+abc
+    </div>
+    <div className="Banner">
+            <img className='BannerLogo' src={rebanner} alt="default Banner"></img>
+
+        </div></>  
+
+);
+
+
+
+}
+export default NavBar;
